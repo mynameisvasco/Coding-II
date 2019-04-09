545e27c0d27ed03ceaa7a30c375e0b999b2b002c
Vasco Sousa <myemailvasco@gmail.com>
2019-04-09 10:02:06 +0100

    Lesson07 //WIP

--- Lesson07/p2utils/DynamicArray.java
+++ Lesson07/p2utils/DynamicArray.java
@@ -13,7 +13,11 @@ public class DynamicArray<E> {
   private static final int BLOCK = 256;
 
   // Define the constructor to initialize the fields
-  //...
+  public DynamicArray()
+  {
+    E[] array = (E[]) new Object[0];
+    this.array = array;
+  }
 
   /** Returns the length of the array.
    * The "length" is defined as 1 plus the maximum index that was set.
@@ -25,7 +29,22 @@ public class DynamicArray<E> {
    */
   public void set(int idx, E value) {
     assert idx >= 0;
-    //...
+    if(idx >= this.array.length)
+    {
+      E[] newArray = (E[]) new Object[this.array.length + this.BLOCK];
+      int i = 0;
+      while(i < this.array.length && this.array[i] != null)
+      {
+        newArray[i] = this.array[i];
+        i++;
+      }
+      this.array = newArray;
+      this.array[idx] = value;
+    }
+    else
+    {
+      this.array[idx] = value;
+    }
   }
 
 
@@ -33,12 +52,34 @@ public class DynamicArray<E> {
    * @param idx  the index of the element (0 is the first).
    * @return value at position idx of the array, or null, if not set yet.
    */
-  public E get(int idx) {
+  public E get(int idx) 
+  {
     assert idx >= 0;
-    //...
+    int i = 0;
+    while(i < this.array.length && this.array[i] != null)
+    {
+      if(i == idx)
+      {
+        return this.array[i];
+      }
+      i++;
+    }
+    return null;
   }
 
   // Define the second get method
-  //...
-
+  public E get(int idx, E d) 
+  {
+    assert idx >= 0;
+    int i = 0;
+    while(i < this.array.length && this.array[i] != null)
+    {
+      if(i == idx)
+      {
+        return this.array[i];
+      }
+      i++;
+    }
+    return d;
+  }
 }
